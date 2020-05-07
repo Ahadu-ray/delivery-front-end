e1a56878bbeffea80327504f1a18fa6d86de7c9b
lidya <lidya.s164@gmail.com>
2020-05-08 01:04:11 +0300

    installed packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./feature-modules/authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./feature-modules/features/features.module').then(m => m.FeaturesModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
