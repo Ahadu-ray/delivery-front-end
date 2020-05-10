b17099fc0d06846b60111808f913e2202270faba
lidya <lidya.s164@gmail.com>
2020-05-10 14:13:05 +0300

    add item list component

---
+++ src/app/feature-modules/features/items/items-list/items-list.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} from '@angular/core';
+import {ItemService} from '../item.service';
+import {NotificationService} from '../../../../shared/services/notification.service';
+import {Item} from '../item.model';
+import {BsModalService} from 'ngx-bootstrap/modal';
+import {UploadItemImageComponent} from '../upload-item-image/upload-item-image.component';
+
+@Component({
+  selector: 'app-items-list',
+  templateUrl: './items-list.component.html',
+  styleUrls: ['./items-list.component.scss']
+})
+export class ItemsListComponent implements OnInit {
+  items: Item[];
+
+  constructor(
+    private itemService: ItemService,
+    private modalservice: BsModalService,
+    private notificationService: NotificationService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.listItems();
+  }
+
+  listItems() {
+    // change list pagination when count is added to resp body
+    this.itemService.listItems().subscribe(result => {
+      if (result) {
+        // change response when resp body is fixed
+        if (result.status === 200) {
+          // console.log(result);
+          this.items = result.body.filter(item => item.imgUrl = window.location.origin + '/' + item.imgUrl);
+        } else {
+          this.notificationService.error(`Couldn't list items`, result.statusText);
+        }
+      }
+    });
+  }
+
+  uploadImage(_id: number) {
+    const initialState = {
+      itemId: _id
+    };
+    this.modalservice.show(UploadItemImageComponent, {initialState});
+    this.modalservice.onHide.subscribe(value => {
+      this.listItems();
+    });
+  }
+}
